082a1b3eac67587ee27626ad2b031bd0a9c36361
MatheusCodonho <144246718+MatheusCodonho@users.noreply.github.com>
2025-03-27 20:22:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Função para exibir uma mensagem quando o botão de contato for clicado
+document.getElementById('contactBtn').addEventListener('click', function() {
+    alert('Obrigado por seu interesse no Santos FC! Em breve entraremos em contato.');
+});
